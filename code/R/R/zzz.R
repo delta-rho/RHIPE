a1728e03fda1d6872d575e96ab72a072bae91cd5
Saptarshi Guha <sguha@ajgubi.(none)>
2013-01-14 13:50:38 -0800

    Added easy way to turn on pRNGs

--- code/R/R/zzz.R
+++ code/R/R/zzz.R
@@ -10,6 +10,34 @@ Mode <-  "experimental"
 
 
 
+
+
+initPRNG <- function(seed=NULL){
+  seed <- eval(seed)
+  mi <- function(){
+    getUID <- function(id = Sys.getenv("mapred.task.id")) {
+      a <- strsplit(id, "_")[[1]]
+      a <- as.numeric(a[c(2, 3, 5)])
+    }
+    library(parallel)
+    RNGkind("L'Ecuyer-CMRG")
+    if(!is.null(iseed)) set.seed(iseed)
+    current.task.number <- getUID()[3]
+    seed <- .Random.seed
+    if (current.task.number > 1) {
+      for (i in 2:current.task.number) seed <- nextRNGStream(seed)
+    }
+    RNGkind("default")
+    assign(".Random.seed", seed, envir = .GlobalEnv)
+    rhcounter("Seed",paste(.Random.seed,collapse=":"),1)
+  }
+  e1 <- new.env(parent = .GlobalEnv)
+  assign("iseed",seed,envir=e1)
+  environment(mi) <- e1
+  mi
+}
+
+
 .onLoad <- function(libname,pkgname){
   library.dynam("Rhipe", pkgname, libname)
   #require(methods)
