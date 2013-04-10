b9459ba478b371c0954d314b98d49f775932beb7
Ryan Hafen <rhafen@gmail.com>
2013-04-10 16:35:24 -0700

    added envir argument to rhsave

--- code/R/R/rhsave.R
+++ code/R/R/rhsave.R
@@ -6,6 +6,7 @@
 #' 
 #' @param \ldots additional parameters for \code{rhsave}
 #' @param file Absolute path to file on HDFS.  Creates the file or overwrites
+#' @param environment to search for objects to be saved
 #'   it.
 #' @author Saptarshi Guha
 #' @return NULL
@@ -26,9 +27,11 @@
 #'   }
 #' }
 #' @export
-rhsave <- function(...,file){
-  on.exit({unlink(x)})
-  x <- tempfile(pattern='rhipe.save')
-  save(...,file=x)
-  rhput(src=x,dest=file)
+rhsave <- function (..., file, envir=parent.frame()) {
+  on.exit({
+    unlink(x)
+  })
+  x <- tempfile(pattern = "rhipe.save")
+  save(..., file = x, envir=envir)
+  rhput(src = x, dest = file)
 }
